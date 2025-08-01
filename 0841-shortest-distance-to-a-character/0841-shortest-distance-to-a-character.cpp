0985e2482303db908f2e31a19234439ee1a4a38d
Tejasav Saxena <Tejasav.18025@stu.upes.ac.in>
2025-08-01 18:13:01 +0530

    Time: 0 ms (100%), Space: 9.4 MB (65.36%) - LeetHub

---
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+         int n = s.length();
+        vector<int> answer(n, n);  
+        int prev = -n;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == c) {
+                prev = i;
+            }
+            answer[i] = i - prev;
+        }
+        prev = 2 * n;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == c) {
+                prev = i;
+            }
+            answer[i] = min(answer[i], prev - i);
+        }
+        return answer;
+    }
+};
